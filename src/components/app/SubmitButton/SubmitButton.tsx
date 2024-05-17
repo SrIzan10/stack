00a0036e7816ad5b70d70e5f30909ab935c0bd27
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2024-05-17 22:19:48 +0200

    fix: submitButton sending buttonText to the dom element

--- src/components/app/SubmitButton/SubmitButton.tsx
+++ src/components/app/SubmitButton/SubmitButton.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { Button, ButtonProps, buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { VariantProps } from "class-variance-authority"
 import { useFormStatus } from "react-dom"
 
 export default function SubmitButton(props: Props) {
     const { pending } = useFormStatus()
+    const { buttonText, ...propsRest } = props
     return (
-        <Button type="submit" loading={pending} {...props}>
+        <Button type="submit" loading={pending} {...propsRest}>
             {props.buttonText}
         </Button>
     )
